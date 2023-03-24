cb6952f0ac992a41e46fb3b2ef06a5cde2691465
AlexandrMakeev <pipifax@bk.ru>
2023-03-24 13:12:28 +0300

    3

--- src/test/java/MaxNumberTest.java
+++ src/test/java/MaxNumberTest.java
@@ -3,10 +3,17 @@ import org.junit.jupiter.api.Test;
 
 public class MaxNumberTest {
     @Test
-    void MaxNumber(){
+    void MaxNumberA(){
         MaxNumberService service = new MaxNumberService();
         int expected = 10;
         int actual = service.max(10, 9);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void MaxnumberB(){
+        MaxNumberService service = new MaxNumberService();
+        int expected = 90;
+        int actual = service.max(50, 90);
         Assertions.assertEquals(expected, actual);
     }
 }
